2b46059983b06a1f2965251a1f3157de9948f8b4
Mohit Raj <mohit@agrmohit.com>
2024-11-26 23:24:08 +0530

    feat: respond with 200 ok

--- app/server.go
+++ app/server.go
@@ -17,9 +17,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	defer l.Close()
+	fmt.Println("Server  is listening on port 4221")
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
